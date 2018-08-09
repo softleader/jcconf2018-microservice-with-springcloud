b71d863e2cc2cd3f789c13edb0055fc29ef260a8
sevenlfanks <godjdc@gmail.com>
2018-08-09 16:31:56 +0800

    init

---
+++ jcconf2018-demo-service/src/main/java/tw/com/softleader/jcconfdemo/service/Jcconf2018DemoServiceApplication.java
@@ -0,0 +1,12 @@
+package tw.com.softleader.jcconfdemo.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jcconf2018DemoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jcconf2018DemoServiceApplication.class, args);
+	}
+}
