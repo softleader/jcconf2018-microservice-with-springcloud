519fdfa75f37de432616fe70772d002acb04acbb
Matt S.Y. Ho <methodho@gmail.com>
2018-08-10 16:05:26 +0800

    update

---
+++ jcconf2018-build-env/README.md
@@ -0,0 +1,6 @@
+# Jcconf2018 Build Env
+
+```sh
+$ docker build -t softleader/build-env:jcconf2018 .
+$ docker push softleader/build-env:jcconf2018
+```
---
+++ jcconf2018-build-env/src/main/java/tw/com/softleader/jcconf2018buildenv/Jcconf2018BuildEnvApplication.java
@@ -0,0 +1,12 @@
+package tw.com.softleader.jcconf2018buildenv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jcconf2018BuildEnvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jcconf2018BuildEnvApplication.class, args);
+	}
+}
