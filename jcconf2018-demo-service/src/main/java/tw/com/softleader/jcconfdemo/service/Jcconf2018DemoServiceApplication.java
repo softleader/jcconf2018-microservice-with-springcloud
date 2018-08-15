d558c6080dc7bdbbf67bce5a17d1ed0808ece35b
sevenlfanks <godjdc@gmail.com>
2018-08-15 12:10:15 +0800

    setting up service

--- jcconf2018-demo-service/src/main/java/tw/com/softleader/jcconfdemo/service/Jcconf2018DemoServiceApplication.java
+++ jcconf2018-demo-service/src/main/java/tw/com/softleader/jcconfdemo/service/Jcconf2018DemoServiceApplication.java
@@ -2,7 +2,9 @@ package tw.com.softleader.jcconfdemo.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Jcconf2018DemoServiceApplication {
 
