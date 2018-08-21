32bca917ffa2d5d407b2ad34e7bc838909ed2ba7
sevenlfanks <godjdc@gmail.com>
2018-08-21 13:58:58 +0800

    update

--- jcconf2018-service/src/main/java/tw/com/softleader/jcconf2018/service/Jcconf2018ServiceApplication.java
+++ jcconf2018-service/src/main/java/tw/com/softleader/jcconf2018/service/Jcconf2018ServiceApplication.java
@@ -2,13 +2,12 @@ package tw.com.softleader.jcconf2018.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class Jcconf2018ServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Jcconf2018ServiceApplication.class, args);
 	}
+
 }
